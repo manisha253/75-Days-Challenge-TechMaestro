26df1bd912c7fe83583946538328d9eae81e3817
manisha253 <69650358+manisha253@users.noreply.github.com>
2022-07-13 13:37:27 +0530

    Time: 9 ms (30.37%), Space: 13.6 MB (5.37%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,17 +12,19 @@
 class Solution {
 public:
     vector<vector<int>> v;
-    void func(TreeNode* root, int l)
+    void func(TreeNode* root,int h)
     {
         if(root==NULL)
+        {
             return;
-        if(v.size()==l)
+        }
+        if(v.size()==h)
         {
             v.push_back({});
         }
-        v[l].push_back(root->val);
-        func(root->left,l+1);
-        func(root->right, l+1);
+        v[h].push_back(root->val);
+        func(root->left,h+1);
+        func(root->right,h+1);
     }
     vector<vector<int>> levelOrder(TreeNode* root)
     {
