c78a4f24a7e1834a74c1ad4f400be29d51f21713
manisha253 <69650358+manisha253@users.noreply.github.com>
2022-07-30 01:03:18 +0530

    Time: 11 ms (23.78%), Space: 8.6 MB (29.81%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool func(string word, string pat)
+    {
+        unordered_map<char,char> patmap;
+        unordered_map<char,char> wordmap;
+        for(int i=0;i<pat.size();i++)
+        {
+            if(wordmap.find(word[i])==wordmap.end())
+            {
+                wordmap[word[i]]=pat[i];
+            }
+            if(patmap.find(pat[i])==patmap.end())
+            {
+                patmap[pat[i]]=word[i];
+            }
+            if(wordmap[word[i]]!=pat[i] || patmap[pat[i]]!=word[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) 
+    {
+        vector<string>res;
+        for(int i=0;i<words.size();i++)
+        {
+            if(func(words[i],pattern))
+            {
+                res.push_back(words[i]);
+            }
+        }
+        return res;
+    }
+};
